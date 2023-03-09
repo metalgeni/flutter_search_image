f492a48f9494f42c9acfb52c800e1c5f978fc893
metalgeni <metalgeni@paran.com>
2023-03-10 03:42:54 +0900

    favorate page

--- lib/model/favorate.dart
+++ lib/model/favorate.dart
@@ -3,31 +3,17 @@ import 'package:flutter/foundation.dart';
 class FavorateModel extends ChangeNotifier {
   FavorateModel() {}
 
-  final List<String> _items = [];
+  final List<String> _favs = [];
 
   void add(String item) {
-    _items.add(item);
+    _favs.add(item);
     notifyListeners();
   }
 
   void remove(String item) {
-    _items.remove(item);
+    _favs.remove(item);
     notifyListeners();
   }
+
+  List<String> get favs => _favs;
 }
-
-
-//search
-/*
-    var isInCart = context.select<CartModel, String>(
-      // Here, we are only interested whether [item] is inside the cart.
-      (cart) => cart.items.contains(item),
-    );
-    */
-
-
-// favorate
-//var cart = context.watch<CartModel>();
-          // onPressed: () {
-          //   cart.remove(cart.items[index]);
-          // },
